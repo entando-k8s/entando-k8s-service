68088adce0d307efbe410f91a7abd27b1f9b7131
Kerruba <luca.cherubin@gmail.com>
2020-01-29 15:25:31 +0100

    Updates dependency on entando-k8s-custom-model

--- src/main/java/org/entando/kubernetes/exception/BadRequestExceptionFactory.java
+++ src/main/java/org/entando/kubernetes/exception/BadRequestExceptionFactory.java
@@ -1,18 +1,18 @@
 package org.entando.kubernetes.exception;
 
 import org.entando.kubernetes.model.plugin.EntandoPlugin;
+import org.entando.web.exception.HttpException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpClientErrorException;
 
 public final class BadRequestExceptionFactory {
 
     private BadRequestExceptionFactory() {
     }
 
-    public static HttpClientErrorException pluginAlreadyDeployed(EntandoPlugin plugin) {
-        return new HttpClientErrorException(HttpStatus.BAD_REQUEST,
-                String.format("Plugin with name %s is already deployed in namespace %s",
-                plugin.getMetadata().getName(), plugin.getMetadata().getNamespace()));
+    public static HttpException pluginAlreadyDeployed(EntandoPlugin plugin) {
+        return new HttpException(HttpStatus.BAD_REQUEST,
+                "org.entando.error.pluginAlreadyDeployed",
+                new String[]{plugin.getMetadata().getName(), plugin.getMetadata().getNamespace()});
     }
 
 
