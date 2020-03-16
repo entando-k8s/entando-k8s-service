9c4c18fd76eebf58c446b2faf7c15d665a74831a
Kerruba <luca.cherubin@gmail.com>
2020-03-16 12:09:09 +0100

    Adds links to the resource assembler for namespace

--- src/main/java/org/entando/kubernetes/service/assembler/NamespaceResourceAssembler.java
+++ src/main/java/org/entando/kubernetes/service/assembler/NamespaceResourceAssembler.java
@@ -1,6 +1,13 @@
 package org.entando.kubernetes.service.assembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import io.fabric8.kubernetes.api.model.Namespace;
+import org.entando.kubernetes.controller.EntandoAppController;
+import org.entando.kubernetes.controller.EntandoDeBundleController;
+import org.entando.kubernetes.controller.EntandoPluginController;
+import org.entando.kubernetes.controller.NamespaceController;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -10,7 +17,17 @@ public class NamespaceResourceAssembler implements
         RepresentationModelAssembler<Namespace, EntityModel<Namespace>> {
 
     @Override
-    public EntityModel<Namespace> toModel(Namespace entity) {
-        return new EntityModel<>(entity);
+    public EntityModel<Namespace> toModel(Namespace ns) {
+
+        EntityModel<Namespace> em = new EntityModel<>(ns);
+
+        String nsName = ns.getMetadata().getName();
+        em.add(linkTo(methodOn(NamespaceController.class).getByName(nsName)).withSelfRel());
+        em.add(linkTo(methodOn(EntandoPluginController.class).listInNamespace(nsName)).withRel("plugins"));
+        em.add(linkTo(methodOn(EntandoAppController.class).listInNamespace(nsName)).withRel("apps"));
+        em.add(linkTo(methodOn(EntandoDeBundleController.class).getAllBundlesInNamespace(nsName)).withRel("bundles"));
+
+        return em;
     }
+
 }
--- src/test/java/org/entando/kubernetes/controller/NamespaceControllerTest.java
+++ src/test/java/org/entando/kubernetes/controller/NamespaceControllerTest.java
@@ -1,6 +1,7 @@
 package org.entando.kubernetes.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.entando.kubernetes.util.EntandoPluginTestHelper.TEST_PLUGIN_NAMESPACE;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -8,16 +9,20 @@ import static org.mockito.Mockito.when;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import org.entando.kubernetes.model.plugin.EntandoPlugin;
 import org.entando.kubernetes.service.NamespaceService;
+import org.entando.kubernetes.service.assembler.EntandoPluginResourceAssembler;
 import org.entando.kubernetes.service.assembler.NamespaceResourceAssembler;
+import org.entando.kubernetes.util.EntandoPluginTestHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.zalando.problem.ThrowableProblem;
@@ -25,13 +30,19 @@ import org.zalando.problem.ThrowableProblem;
 @Tag("component")
 public class NamespaceControllerTest {
 
+    private EntandoPluginController pluginController;
+    private EntandoPluginResourceAssembler pluginResourceAssembler = new EntandoPluginResourceAssembler();
+
     private NamespaceController nsController;
     private NamespaceResourceAssembler nsRa;
     private NamespaceService nsService;
 
+
+
     @BeforeEach
     public void setup() {
         nsService = mock(NamespaceService.class);
+        pluginController = mock(EntandoPluginController.class);
         nsRa = new NamespaceResourceAssembler();
         nsController = new NamespaceController(nsRa, nsService);
     }
@@ -58,11 +69,32 @@ public class NamespaceControllerTest {
        assertThat(re.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
        assertThat(re.getBody()).isNotNull();
        assertThat(re.getBody().getLinks()).isNotNull();
-       assertThat(re.getBody().getLinks().getLink("self").isPresent());
+       assertThat(re.getBody().getLinks().getLink("self").isPresent()).isTrue();
     }
 
+    @Test
+    public void shouldReturnLinksToEntandoCustomResourceInNamespace() {
+        CollectionModel<EntityModel<EntandoPlugin>> epList = new CollectionModel<>(
+               testPluginList().stream().map(pluginResourceAssembler::toModel).collect(Collectors.toList())
+        );
+        when(nsService.getObservedNamespace(TEST_PLUGIN_NAMESPACE)) .thenReturn(Optional.of(testNamespace()));
+        when(pluginController.listInNamespace(TEST_PLUGIN_NAMESPACE)).thenReturn(ResponseEntity.ok(epList));
+
+        ResponseEntity<EntityModel<Namespace>> re = nsController.getByName(TEST_PLUGIN_NAMESPACE);
+        assertThat(re.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(re.getBody()).isNotNull();
+        assertThat(re.getBody().getLinks()).isNotNull();
+        assertThat(re.getBody().getLinks().getLink("plugins").isPresent()).isTrue();
+        assertThat(re.getBody().getLinks().getLink("apps").isPresent()).isTrue();
+        assertThat(re.getBody().getLinks().getLink("bundles").isPresent()).isTrue();
+
+    }
 
     private Namespace testNamespace() {
         return new NamespaceBuilder().withNewMetadata().withName("my-name").endMetadata().build();
+    }
+
+    private List<EntandoPlugin>  testPluginList() {
+        return Collections.singletonList(EntandoPluginTestHelper.getTestEntandoPlugin());
     }
 }
