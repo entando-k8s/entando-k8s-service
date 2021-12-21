0c641207bba440e23065220abde32d782f93eeae
firegloves <lucorset@gmail.com>
2021-12-21 09:37:00 +0100

    ENG-3061 log4j version to pom variable

--- src/test/java/org/entando/kubernetes/util/CustomKubernetesServer.java
+++ src/test/java/org/entando/kubernetes/util/CustomKubernetesServer.java
@@ -10,8 +10,8 @@ import okhttp3.TlsVersion;
 
 public class CustomKubernetesServer extends KubernetesServer {
 
-    public static final TlsVersion[] TLS_VERSIONS =
-            {TlsVersion.TLS_1_0, TlsVersion.TLS_1_1, TlsVersion.TLS_1_2, TlsVersion.TLS_1_3};
+    public static final TlsVersion[] TLS_VERSIONS = {TlsVersion.TLS_1_0, TlsVersion.TLS_1_1, TlsVersion.TLS_1_2,
+            TlsVersion.TLS_1_3};
     private NamespacedKubernetesClient clientOverride;
 
     public CustomKubernetesServer(boolean https, boolean crudMode) {
@@ -19,16 +19,12 @@ public class CustomKubernetesServer extends KubernetesServer {
     }
 
     @Override
-    synchronized public NamespacedKubernetesClient getClient() {
+    public synchronized NamespacedKubernetesClient getClient() {
         if (clientOverride == null) {
             ConfigBuilder cb = new ConfigBuilder();
-            Config config = cb.withMasterUrl(this.getMockServer().url("/").toString())
-                    .withTrustCerts(true)
-                    .withTlsVersions(TLS_VERSIONS)
-                    .withNamespace("test")
-                    .build();
-            clientOverride = new DefaultKubernetesClient(HttpClientUtils.createHttpClientForMockServer(config),
-                    config);
+            Config config = cb.withMasterUrl(this.getMockServer().url("/").toString()).withTrustCerts(true)
+                    .withTlsVersions(TLS_VERSIONS).withNamespace("test").build();
+            clientOverride = new DefaultKubernetesClient(HttpClientUtils.createHttpClientForMockServer(config), config);
         }
         return clientOverride;
     }
