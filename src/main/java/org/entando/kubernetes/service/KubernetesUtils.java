8027dfcadbf5fc96a513dccc10dbdd0d8cb1f6aa
Federico Gallucci <zyzyx85@gmail.com>
2024-12-10 10:20:20 +0100

    add more logging

--- src/main/java/org/entando/kubernetes/service/KubernetesUtils.java
+++ src/main/java/org/entando/kubernetes/service/KubernetesUtils.java
@@ -81,7 +81,7 @@ public class KubernetesUtils implements JwtDecoder {
 //        return this.kubernetesClients.get(DefaultKubernetesClientBuilder.NOT_K8S_TOKEN);
         //If we ever require serviceAccount propagation from component-manager, reactivate this line:
         //return this.kubernetesClients.get(currentToken.get());
-        Logger.getLogger(getClass().getName()).log(Level.INFO, namespacesToObserve);
+        Logger.getLogger(getClass().getName()).log(Level.INFO, "NAMESPACE_TO_OBSERVE: "+namespacesToObserve);
         return getCurrentKubernetesClient(clusterAddress,clusterToken, namespacesToObserve);
     }
 
